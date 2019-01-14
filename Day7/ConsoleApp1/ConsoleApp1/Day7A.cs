fef2ba8f700042ce1bbdb3fcdee0dedce665fac9
Kristoffer Jälén <kristoffer.jalen@tretton37.com>
2019-01-14 11:47:56 +0100

    Add day 7 and 8

---
+++ Day7/ConsoleApp1/ConsoleApp1/Day7A.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Day7A
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt");
+            
+            var steps = lines
+                .Select(x => new
+                {
+                    Step = x.Substring(x.IndexOf(" can") - 1, 1),
+                    Requires = x.Substring(x.IndexOf(" must") - 1, 1)
+                })
+                .GroupBy(x => x.Step,
+                    x => x.Requires,
+                    (key, g) => new { Step = key, Requires = g.ToList() })
+                .ToDictionary(x => x.Step, x => x.Requires);
+
+            var allsteps = steps
+                .Values
+                .SelectMany(x => x)
+                .Concat(steps.Keys)
+                .Distinct()
+                .OrderBy(x => x)
+                .ToList();
+
+            var finished = new List<string>();
+
+            var i = 0;
+            while (i < allsteps.Count && allsteps.Count > 0)
+            {
+                var step = allsteps[i++];
+                if (steps.ContainsKey(step) && !steps[step].All(x => finished.Contains(x)))
+                    continue;
+
+                finished.Add(step);
+                allsteps.Remove(step);
+                i = 0;
+            }
+
+            var result = string.Join("", finished.Select(x => x));
+
+            Console.WriteLine(result); // ACHOQRXSEKUGMYIWDZLNBFTJVP
+        }
+    }
+}
---
+++ Day8/ConsoleApp1/ConsoleApp1/Day8A.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Day8A
+    {
+        private static int _index;
+        private static int[] _tree;
+
+        static void Main(string[] args)
+        {
+            var text = File.ReadAllText("input.txt");
+
+            _tree = text.Split(' ').Select(int.Parse).ToArray();
+
+            var sum = Rec(0); // 41555
+
+            Console.WriteLine(sum);
+        }
+
+        private static int Rec(int sum)
+        {
+            var c = _tree[_index + 0];
+            var m = _tree[_index + 1];
+
+            _index += 2;
+
+            for (var i = 0; i < c; i++)
+            {
+                sum = Rec(sum);
+            }
+
+            var metas = _tree.Skip(_index).Take(m);
+
+            _index = _index + m;
+
+            return sum + metas.Sum();
+        }
+    }
+}
